7c2d73e554f7196b78e70af83e61a7f47769da0c
Nathan Swulinski nswulinski@gmail.com <nswulinski@gmail.com>
2023-08-15 08:59:39 +0200

    blah blah blah

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,9 +4,10 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:input]
-
     if @question == "I am going to work"
       @answer = "Great!"
+    elsif @question == "more tea victor?"
+      @answer = "yum yum"
     elsif @question.end_with?("?")
       @answer = "Silly question, get dressed and go to work!"
     else
